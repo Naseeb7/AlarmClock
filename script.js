3f6b0508ecd450acbd555280914b0f6d3c17fe7e
Naseeb7 <chandpradosh7@gmail.com>
2023-03-25 12:09:44 +0530

    Script, stylesheet and audio file added

---
+++ script.js
@@ -0,0 +1,16 @@
+const play=()=>{
+    let audio=new Audio("mixkit-digital-clock-digital-alarm-buzzer-992.wav")
+    audio.play()
+}
+set.onclick=(e)=>{
+    e.preventDefault()
+    let time=document.getElementById("time").value
+    if(time.length>=1){
+    setTimeout(()=>{
+        play()
+    },time*1000)
+    }
+    else{
+        alert("Enter time interval in seconds please!!")
+    }
+}
